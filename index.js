1fa5ae0ed723ae100f7628a8e94566a49f5b360f
IanSylvester <sylvester16@gmail.com>
2023-03-28 19:20:58 -0400

    added ability to add new items to cart and update ones already there

--- index.js
+++ index.js
@@ -99,6 +99,17 @@ const generatePOSItems = () => {
       // YOU WILL NEED TO ADD CODE HERE
       // NOTE: this button currently has context for
       // the booster variable from the forEach() scope
+      const item = { booster, quantity: 1 };
+      const productInCart = cart.find(
+        (cartProduct) => cartProduct.booster.id === item.booster.id
+      );
+
+      if (productInCart === undefined) {
+        cart.push(item);
+      } else {
+        productInCart.quantity += 1;
+      }
+      console.log(cart)
     });
 
     posItems.append(h4);
